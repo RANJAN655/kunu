a4f98ded7cc22c004915b77c5f699b5d485ebaf4
RANJAN655 <ranjankumar976666@gmail.com>
2025-06-20 19:44:28 +0900

    add one.js

--- one.js
+++ one.js
@@ -1,3 +1,7 @@
 console.log("ranjan kumar nayak");
 console.log("this terminal is workin ")
 console.log("special check case");
+const kunu = 87
+const rakes ="rakes"
+console.log("2" === 2)
+console.log("9" * 2);
